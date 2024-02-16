7f97a32088035262b642afdffa9f944f63e4200e
Anakhsup <145113685+Anakhsup@users.noreply.github.com>
2024-02-16 15:30:16 +0700

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,49 +1,39 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
+#include <vector>
 
-int size = 10000000;
+#ifdef DOUBLE 
+#define tp double
+#endif
 
-int main() {
-    double* mass;
-    mass = (double*)malloc(size * sizeof(double));
-    
-    for (int i = 0; i < size; i++) {
-        mass[i] = sin(i * 2 * M_PI / size);
+#ifdef FLOAT
+#define tp float
+#endif
+
+
+int main(int argc, char** argv) {
+    std::vector<tp> arr(10000000);
+
+#ifdef DOUBLE
+    tp res = 0.0;
+
+    for (int x = 0; x < 10000000; ++x) {
+        arr[x] = sin(2 * M_PI * x / 10000000);
+        res += sin(arr[x]);
     }
+    std::cout << "Double output " << res << "\n";
+#endif
 
-    double sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += mass[i];
+#ifdef FLOAT
+    tp res = 0.0f;
+
+    for (int x = 0; x < 10000000; ++x) {
+        arr[x] = sin(2 * M_PI * x / 10000000);
+        res += sinf(arr[x]);
     }
-
-    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Float output " << res << "\n";
+#endif
 
     return 0;
 }
-
-//g++ 1.cpp
-//./a.out
-//Sum: 4.89582e-11
-
-//прописать make float или make double
-//g++ -std=c++11 -DARRAY_TYPE=float -o program_float 1.cpp
-//g++ -std=c++11 -DARRAY_TYPE=double -o program_double 1.cpp
-
-//./program_double 
-//Sum: 4.89582e-11
-
-//./program_float 
-//Sum: 4.89582e-11
-
-
-//mkdir build
-//cd build
-//cmake -DARRAY_TYPE=float ..
-//make
-
-//./program_double 
-//Sum: 6.27585e-10
-
-//./program_float 
-//Sum: 6.27585e-10
-
